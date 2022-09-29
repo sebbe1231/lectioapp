46ad0b62016522e2e06d471f29468de32e03e6b2
Sebastian <86983004+sebbe1231@users.noreply.github.com>
2022-09-29 10:08:18 +0200

    Added more shtuff and cleaned it up a bit
    and no i will not linebreak on line 38, legit cry about it

--- lectioapp.py
+++ lectioapp.py
@@ -12,41 +12,45 @@ except exceptions.IncorrectCredentialsError:
     print("Not authenticated!")
     exit(1)
 
-
+    
 @click.group()
 def lectioapp():
     pass
 
-def user_table(user_item) -> None:
+
+def user_table(user_item: list) -> None:
     """Generates a user table based on user list"""
     print(cooltables.create_table([
         ["Navn", "Klasse", "Initialer", "Skole", "Type", "Lectio ID"],
         *[[str(i.name), str(i.class_name), str(i.initials), lect.school().name, i.type.get_str(), str(i.id)] for i in user_item]
     ], theme=cooltables.ROUNDED_THEME))
 
-def schedule_table(sched_item) -> None:
+
+def schedule_table(sched_item: list) -> None:
     """Generates a schedule table based on schedule list"""
-    
+
     # Status codes
     status = ["Unchanged", "Changed", "cancelled"]
 
     print(cooltables.create_table([
-        ["Fag", "Lokale", "Lærer", "Start", "Slut", "Status"],
-        *[[str(i.subject), str(i.room)[:5], str(i.teacher), str(i.start_time), str(i.end_time), status[int(i.status)]] for i in sched_item]
+        ["Fag", "Titel", "Lokale", "Lærer", "Start", "Slut", "Længde", "Status"],
+        # No im not gonna line break, cope
+        *[[(str(i.subject) if len(str(i.subject)) <= 18 else f"{str(i.subject)[:18]}..."), (str(i.title) if len(str(i.title)) <= 16 else f"{str(i.title)[:16]}..."),str(i.room)[:5], str(i.teacher), str(i.start_time)[:-3], str(i.end_time)[:-3], str(i.end_time-i.start_time), status[int(i.status)]] for i in sched_item]
     ], theme=cooltables.ROUNDED_THEME))
 
 @lectioapp.command()
 def now():
     """Get the current ongoing class"""
     print(f"Current time: {str(datetime.now())[:-7]} \n")
-    try:
-        x = lect.me().get_schedule(start_date=datetime.now(),
-                                   end_date=datetime.now()+timedelta(seconds=1), strip_time=False)
-    except AttributeError:
+    x = lect.me().get_schedule(start_date=datetime.now(),
+                                end_date=datetime.now()+timedelta(seconds=1), strip_time=False)
+    if len(x) == 0:
         print("No ongoing modules. Use \"lectioapp next\" to see the next module")
         exit(1)
 
     schedule_table(x)
+
+    print(f"Total school hours:\n{x[0].end_time-x[0].start_time}")
 
 @lectioapp.command()
 @click.argument('date', required=False)
@@ -65,26 +69,26 @@ def day(date):
             print("Not valid time/date")
             exit(1)
     else:
-        date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        date = datetime.now()
 
     # Check if there are any classes for that day
-    try:
-        x = lect.me().get_schedule(start_date=date, end_date=date +
-                                   timedelta(days=1), strip_time=False)
-    except AttributeError:
+    x = lect.me().get_schedule(start_date=date, end_date=date, strip_time=True)
+    if len(x) == 0:
         print(f"No modules on the date {str(date)[:-8]}")
         exit(1)
 
     schedule_table(x)
 
+    print(f"Total school hours:\n{x[-1].end_time-x[0].start_time}")
+
 @lectioapp.command()
 def next():
     """Get the next class in line"""
-    
+
     # h is hours to add to date
     h = 1
     while True:
-        # If the last class in the list has already been started, the loop will repeat, adding an extra hour  
+        # If the last class in the list has already been started, the loop will repeat, adding an extra hour
         x = lect.me().get_schedule(start_date=datetime.now(),
                                    end_date=datetime.now()+timedelta(hours=h), strip_time=False)
         if x[-1].start_time <= datetime.now():
@@ -93,13 +97,59 @@ def next():
             break
 
     status = ["Unchanged", "Changed", "Cancelled"]
-
     print(cooltables.create_table([
-        ["Fag", "Lokale", "Lærer", "Start", "Slut", "Status"],
-        [str(x[-1].subject), str(x[-1].room)[:5], str(x[-1].teacher),
-         str(x[-1].start_time), str(x[-1].end_time), status[int(x[-1].status)]]
+        ["Fag", "Titel", "Lokale", "Lærer", "Start", "Slut", "Længde", "Status"],
+        [(str(x[-1].subject) if len(str(x[-1].subject)) <= 18 else f"{str(x[-1].subject)[:18]}..."), 
+         (str(x[-1].title) if len(str(x[-1].title)) <= 16 else f"{str(x[-1].title)[:16]}..."),
+         str(x[-1].room)[:5], str(x[-1].teacher),
+         str(x[-1].start_time)[:-3], str(x[-1].end_time)[:-3], 
+         str(x[-1].end_time-x[-1].start_time),
+         status[int(x[-1].status)]]
     ], theme=cooltables.ROUNDED_THEME))
 
+    print(f"Total school hours:\n{x[-1].end_time-x[-1].start_time}")
+
+@lectioapp.command()
+@click.argument('date', required=False)
+def week(date):
+    """Get the all the classes for the whole week that a given date is within. 
+    \b
+    If no date is specified, the current week will be used
+    \b
+    
+    Costume date in format {year}-{month}-{day}"""
+    
+    if date:
+        try:
+            date = datetime.strptime(date, "%Y-%m-%d")
+        except:
+            print("Not valid time/date")
+            exit(1)
+    else:
+        date = datetime.now()
+    
+    # Define start and end to make it easier to look at
+    start = datetime.now() - timedelta(days=datetime.now().weekday())
+    end = start + timedelta(days=6)
+    # Same as in user -w
+    x = lect.me().get_schedule(start_date=start, end_date=end, strip_time=False)
+
+    # Check if there are actually modules in the list
+    if len(x) == 0:
+        print(f"No modules on the date {str(date)[:-8]}")
+        exit(1)
+
+    schedule_table(x)
+
+    # I have to use this way, by adding each module length to eachother
+    # Or else the wrong length will be displayed
+    i = 0
+    for m in x:
+        if i == 0:
+            i = m.end_time - m.start_time
+        else:
+            i = i + (m.end_time - m.start_time)
+    print(i)
 
 @lectioapp.command()
 @click.argument("user_id", required=False)
@@ -108,7 +158,7 @@ def next():
 @click.option("-w", "--week", help="Get the schedual of the student, for the week", is_flag=True)
 def user(user_id: str, now: bool, day: bool, week: bool):
     """Search for users with ID, if no user id entered, your own profile will be displayed"""
-    
+
     # defining the user (u)
     u = lect.me()
     if user_id:
@@ -122,7 +172,11 @@ def user(user_id: str, now: bool, day: bool, week: bool):
                 print("No user with such id!")
                 exit(1)
 
-    user_table(u)
+    # Not using user_table since the user returned is not a list, but a single user
+    print(cooltables.create_table([
+        ["Navn", "Klasse", "Initialer", "Skole", "Type", "Lectio ID"],
+        [str(u.name), str(u.class_name), str(u.initials), lect.school().name, u.type.get_str(), str(u.id)]
+    ], theme=cooltables.ROUNDED_THEME))
     print(f"Lectio billede:\n{u.image}\n")
 
     # Set start and end date for if student or teachers schedule is asked for
@@ -135,10 +189,23 @@ def user(user_id: str, now: bool, day: bool, week: bool):
     elif week:
         start = datetime.now() - timedelta(days=datetime.now().weekday())
         end = start + timedelta(days=6)
+    else: 
+        exit(1)
 
     x = u.get_schedule(start_date=start, end_date=end, strip_time=False)
 
     schedule_table(x)
+
+    # Incase of the week flag, a forloop will get a better result of total hours than simply doing first and last module
+    i = 0
+    for m in x:
+        if i == 0:
+            i = m.end_time - m.start_time
+        else:
+            i = i + (m.end_time - m.start_time)
+    
+    print(f"Total school hours:\n{i}")
+        
 
 @lectioapp.command()
 @click.argument('term')
@@ -154,7 +221,7 @@ def search(term, student: bool, teacher: bool):
         users = lect.school().search_for_teachers(query=term)
     else:
         users = lect.school().search_for_users(term)
-    
+
     user_table(users)
 
 
