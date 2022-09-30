83f1e101d16e4d89f62f07931bb2e7f5adb6595f
Sebastian <86983004+sebbe1231@users.noreply.github.com>
2022-09-30 11:26:58 +0200

    added overview

--- lectioapp.py
+++ lectioapp.py
@@ -61,7 +61,6 @@ def schedule_table(sched_item: list) -> None:
         ])
     print(cooltables.create_table([
         ["Fag", "Titel", "Lokale", "Lærer", "Start", "Slut", "Længde", "Status"],
-        # No im not gonna line break, cope
         *table
     ], theme=cooltables.ROUNDED_THEME))
 
@@ -113,24 +112,23 @@ def day(date):
 
 @lectioapp.command()
 def next():
-    """Get the next class in line"""
+    """Get the next class of the day"""
 
-    # h is hours to add to date
-    h = 1
-    while True:
-        # If the last class in the list has already been started, the loop will repeat, adding an extra hour
-        x = lect.me().get_schedule(start_date=datetime.now(),
-                                   end_date=datetime.now()+timedelta(hours=h), strip_time=False)
-        if x[-1].start_time <= datetime.now():
-            h += 1
-        else:
-            break
+    now = datetime.now()
+    sched = lect.me().get_schedule(now, now+timedelta(days=5), False)
 
-    status = ["Unchanged", "Changed", "Cancelled"]
-    schedule_table([x[-1]])
-
-    print(f"Total school hours:\n{x[-1].end_time-x[-1].start_time}")
-
+    if len(sched) == 0:
+        print("No modules found in your schedule!")
+        return
+    
+    for i in sched:
+        if i.start_time > now:
+            schedule_table([i])
+            print(f"Total school hours:\n{i.end_time-i.start_time}")
+            return
+    else:
+        print("No future modules found in your schedule, for the next 5 days!")
+        return
 
 @lectioapp.command()
 @click.argument('date', required=False)
@@ -225,19 +223,44 @@ def user(user_id: str, now: bool, day: bool, week: bool):
 
     print(f"Total school hours:\n{i}")
 
+@lectioapp.command()
+def overview():
+    """Get an overview of the current day"""
+    sched = lect.me().get_schedule(datetime.now(), datetime.now(), True)
+
+    # The cache is a dictionary of all the classes of the day, plus the amount of times they show up
+    cache = {}
+    for c in sched:
+        count = cache.get(c.subject)
+        if not count:
+            cache[c.subject] = len(list(filter(lambda i: i.subject == c.subject, sched)))
+            count = cache.get(c.subject)
+
+    schedule_table(sched)
+    print(f"Time untill school is over:\n{str(sched[-1].end_time-datetime.now())[:-7]}")
+    
+    print(f"\nThe distribution of classes for the day:")
+    print(cooltables.create_table([
+        ["Fag", "Moduler", "Procent"],
+        *[[str(subject), 
+           str(times), 
+           f"{times / sum(cache.values()) * 100}%"] for subject, times in cache.items()]
+    ], theme=cooltables.ROUNDED_THEME))
 
 @lectioapp.command()
 @click.argument('term')
 @click.option("-s", "--student", help="Only search for students", is_flag=True)
-@click.option("-t", "--teacher", help="Only search for teachers", is_flag=True)
+@click.option("-t", "--teacher", help="Only search for teachers. Used to search for initials", is_flag=True)
 def search(term, student: bool, teacher: bool):
     """Search for students for teachers"""
     users = []
 
+    lect.school()
+
     if student and not teacher:
         users = lect.school().search_for_students(query=term)
     elif teacher and not student:
-        users = lect.school().search_for_teachers(query=term)
+        users = lect.school().search_for_teachers(query_name=term, query_initials=term)
     else:
         users = lect.school().search_for_users(term)
 
